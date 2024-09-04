ad57bef38c6cceee762473c68c2ba1b91eb2f4e6
curio13 <manishpndy527@gmail.com>
2024-09-04 19:31:36 +0530

    final commit before deploying

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,10 @@ function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   const questions = [
-    "How satisfied are you with our products? (Rating type, 1-5)",
-    "How fair are the prices compared to similar retailers? (Rating type, 1-5)",
-    "How satisfied are you with the value for money of your purchase? (Rating type, 1-5)",
-    "On a scale of 1-10 how would you recommend us to your friends and family? (Rating Type, 1-10)",
+    "How satisfied are you with our products? ",
+    "How fair are the prices compared to similar retailers? ",
+    "How satisfied are you with the value for money of your purchase? ",
+    "On a scale of 1-10 how would you recommend us to your friends and family? ",
     "What could we do to improve our service? "
   ];
 
